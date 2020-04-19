bb0a690c531841ae248afc3b985154bb073261ac
po4inix <63730112+po4inix@users.noreply.github.com>
2020-04-19 10:39:06 +0300

    Add files via upload
    escape simbols table

---
+++ tabl escape.py
@@ -0,0 +1,9 @@
+print('\\a\t\t','Bell (alert)', sep = ' - ', end = '\n')
+print('\\b\t\t','Backspace', sep = ' - ', end = '\n')
+print('\\n\t\t','New Line', sep = ' - ', end = '\n')
+print('\\t\t\t','Horizontal Line', sep = ' - ', end = '\n')
+print('\\\\    \t','Backslash \\',sep = ' - ', end = '\n')
+print('\\"\t\t','Double quotation mark "', sep = ' - ', end = '\n')
+print('\\\'\t\t','Single quotation mark \'', sep = ' - ', end = '\n')
+
+
